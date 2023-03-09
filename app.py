b888ff4772a9df2d235ac45f8a1dada0b4f22743
Nour Siwar <nour.siwar@outlook.com>
2023-03-08 20:55:37 -0800

    updated html, and css

--- app.py
+++ app.py
@@ -15,7 +15,11 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('introduction.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
