78d918f3e139aaf402e8a8fe447e469b64826a30
Nour Siwar <100004210+NourdotSiwar@users.noreply.github.com>
2023-03-08 16:17:58 -0800

    erased predict_api function

--- app.py
+++ app.py
@@ -22,16 +22,5 @@ def predict():
 
     return render_template('index.html', prediction_text='Do you need treatment? {}'.format(output))
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
-
 if __name__ == "__main__":
     app.run(debug=True)
